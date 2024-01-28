e3e56a1431f0f0f7e9d247220164d2c3fa52ea7a
Serhii Filatov <filser.spirit@gmail.com>
2024-01-28 21:19:48 +0200

    adds REST API

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,12 +1,61 @@
-import { listContacts } from '../services/contactsServices.js';
+import {
+  listContacts,
+  getContactById,
+  removeContactById,
+  addContact,
+  updateContactById,
+} from '../services/contactsServices.js';
 
-export const getAllContacts = (req, res) => {};
+import HttpError from '../helpers/HttpError.js';
+import ctrlWrapper from '../helpers/ctrlWrapper.js';
 
-export const getOneContact = (req, res) => {};
+export const getAllContacts = ctrlWrapper(async (req, res) => {
+  const result = await listContacts();
+  res.status(200).json(result);
+});
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = ctrlWrapper(async (req, res) => {
+  const { id } = req.params;
+  const contact = await getContactById(id);
 
-export const createContact = (req, res) => {};
+  if (!contact) {
+    throw HttpError(404, 'Not Found');
+  }
 
-export const updateContact = (req, res) => {};
+  res.status(200).json(contact);
+});
+
+export const deleteContact = ctrlWrapper(async (req, res) => {
+  const { id } = req.params;
+
+  const contact = await removeContactById(id);
+
+  if (!contact) {
+    throw HttpError(404, 'Not Found');
+  }
+
+  res.status(200).json(contact);
+});
+
+export const createContact = ctrlWrapper(async (req, res) => {
+  const newContact = await addContact(req.body);
+
+  res.status(201).json(newContact);
+});
+
+export const updateContact = ctrlWrapper(async (req, res) => {
+  const hasAtLeastOneField = Object.keys(req.body).length > 0;
+  if (!hasAtLeastOneField) {
+    throw HttpError(400, 'Body must have at least one field');
+  }
+
+  const { id } = req.params;
+  const updatedContact = await updateContactById(id, req.body);
+
+  if (!updatedContact) {
+    throw HttpError(404, 'Not found');
+  }
+
+  return res.status(200).json(updatedContact);
+});
 
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -2,7 +2,7 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 import crypto from 'node:crypto';
 
-const contactsPath = path.join('db', 'contacts.json');
+const contactsPath = path.join('db', '/contacts.json');
 
 export async function listContacts() {
   const contactsArr = JSON.parse(
@@ -16,10 +16,11 @@ export async function getContactById(contactId) {
   const contacts = await listContacts();
 
   const contact = contacts.find(contact => contact.id === contactId);
+
   return contact || null;
 }
 
-export async function removeContact(contactId) {
+export async function removeContactById(contactId) {
   const contacts = await listContacts();
 
   const index = contacts.findIndex(contact => contact.id === contactId);
@@ -27,23 +28,38 @@ export async function removeContact(contactId) {
   if (index === -1) {
     return null;
   }
+
   const result = contacts.splice(index, 1);
+
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
   return result;
 }
 
-export async function addContact(name, email, phone) {
-  const contactsArr = await listContacts();
+export async function addContact(body) {
+  const contacts = await listContacts();
+
   const newContact = {
     id: crypto.randomUUID(),
-    name,
-    email,
-    phone,
+    ...body,
   };
 
-  contactsArr.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contactsArr));
-
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return newContact;
+}
+
+export async function updateContactById(id, data) {
+  const contacts = await listContacts();
+
+  const contactIndex = contacts.findIndex(contact => contact.id === id);
+
+  if (contactIndex === -1) {
+    return null;
+  }
+
+  const contactToUpdate = contacts.find(contact => contact.id === id);
+  contacts[contactIndex] = { id, ...contactToUpdate, ...data };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[contactIndex];
 }
